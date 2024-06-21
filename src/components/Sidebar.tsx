510847cadd6804550d2135b554c3f3dbb09c95f7
Hassan.Ragab <Hassan.Ragab@thermofisher.com>
2024-06-21 17:31:51 +0200

    chore: Add components for header, container, sidebar, and job item content

--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -1,7 +1,17 @@
+import JobList from "./JobList";
+import Pagination from "./PaginationControls";
+import ResultsCount from "./ResultsCount";
+import Sorting from "./SortingControls";
+
 export default function Sidebar() {
   return (
     <div className="sidebar">
-      <div className="sidebar__top"></div>
+      <div className="sidebar__top">
+        <ResultsCount />
+        <Sorting />
+      </div>
+      <JobList />
+      <Pagination />
     </div>
   );
 }
